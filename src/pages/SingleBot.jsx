f62f4f45d66de35e7687217d4b46b0cef2613b54
Moses <mochengo29@gmail.com>
2023-07-29 14:08:43 +0300

    Implemented fetching of one bot

--- src/pages/SingleBot.jsx
+++ src/pages/SingleBot.jsx
@@ -3,17 +3,22 @@ import { Link, useParams } from "react-router-dom";
 
 function SingleBot() {
   const { id } = useParams();
-  const [selectedBot, setSelectedBot] = useState();
+  const [selectedBot, setSelectedBot] = useState({});
 
   useEffect(() => {
     async function fetchBot() {
-      const res = await fetch("http://localhost:8000/bots");
+      const res = await fetch(`http://localhost:8000/bots/${id}`);
+      const data = await res.json();
+      setSelectedBot(data);
+      console.log(data);
     }
+    fetchBot();
   }, []);
   return (
     <div>
-      {id}id
-      <Link to="/">Back</Link>
+      <img src={selectedBot.avatar_url} alt="" />
+      <h1>{selectedBot.name}</h1>
+      <Link to="/">BACKKK</Link>
     </div>
   );
 }
